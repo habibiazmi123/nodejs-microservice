710249f20e72fba294631bc80039dd35dc905cbd
mha <mha@voxteneo.com>
2025-06-15 08:24:15 +0700

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+import {app} from './app';
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to Mongodb')
+  } catch (err) {
+    console.log(err)
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+}
+
+start();
