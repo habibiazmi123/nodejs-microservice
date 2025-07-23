90066191650d3161c42a225739a0d8ab9ab16873
mha <mha@voxteneo.com>
2025-07-23 15:02:16 +0700

    section 17: cross service data replication in action

---
+++ tickets/auth/src/routes/signout.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { currentUser } from '@cumidev/common';
+
+const router = express.Router();
+
+router.post('/api/users/signout',currentUser, (req, res) => {
+  req.session = null;
+  res.send({});
+});
+
+export { router as signoutRouter };
---
+++ tickets/client/pages/banana.js
@@ -0,0 +1,9 @@
+const BananaPage = () => {
+    return (
+        <div>
+            <h1>Banana 2</h1>
+        </div>
+    );
+};
+
+export default BananaPage;
